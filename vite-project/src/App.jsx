a0b88e45f51af052e45eec1a2c7352f86c094c0e
rs691 <robstewart@my365.bellvue.edu>
2024-10-16 18:55:19 -0500

    added main files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MainParallax from './components/MainParallax'
+
+function App() {
+ 
+
+  return (
+    <>
+      <MainParallax />
+    </>
+  )
+}
+
+export default App
